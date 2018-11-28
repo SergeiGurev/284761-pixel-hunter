7062d1f73b2d858f31c1c9a1723f0860a214dd18
SergeiGurev <sergeigurev@gmail.com>
2018-11-28 20:31:54 +0700

    переписал шаблон игры с использованием resize

--- js/templates/game.js
+++ js/templates/game.js
@@ -1,62 +1,39 @@
+import resize from '../utils/resize/resize.js';
+import {levelTypes} from '../data/game-data.js';
+
 const renderGame = (level) => {
-  switch (level.type) {
-    case 1:
-      return `<p class="game__task">${level.text}</p>
-      <form class="game__content">
-        <div class="game__option">
-          <img src=${level.images[0].url} alt="Option 1" width="468" height="458">
-          <label class="game__answer game__answer--photo">
-            <input class="visually-hidden" name="question1" type="radio" value="photo">
-            <span>Фото</span>
-          </label>
-          <label class="game__answer game__answer--paint">
-            <input class="visually-hidden" name="question1" type="radio" value="paint">
-            <span>Рисунок</span>
-          </label>
-        </div>
-        <div class="game__option">
-          <img src=${level.images[1].url} alt="Option 2" width="468" height="458">
-          <label class="game__answer  game__answer--photo">
-            <input class="visually-hidden" name="question2" type="radio" value="photo">
-            <span>Фото</span>
-          </label>
-          <label class="game__answer  game__answer--paint">
-            <input class="visually-hidden" name="question2" type="radio" value="paint">
-            <span>Рисунок</span>
-          </label>
-        </div>
-      </form>`;
-    case 2:
-      return `<p class="game__task">${level.text}</p>
-      <form class="game__content  game__content--wide">
-        <div class="game__option">
-          <img src=${level.images[0].url} alt="Option 1" width="705" height="455">
-          <label class="game__answer  game__answer--photo">
-            <input class="visually-hidden" name="question1" type="radio" value="photo">
-            <span>Фото</span>
-          </label>
-          <label class="game__answer  game__answer--paint">
-            <input class="visually-hidden" name="question1" type="radio" value="paint">
-            <span>Рисунок</span>
-          </label>
-        </div>
-      </form>`;
-    case 3:
-      return `<p class="game__task">${level.text}</p>
-      <form class="game__content  game__content--triple">
-        <div class="game__option">
-          <img src=${level.images[0].url} alt="Option 1" width="304" height="455">
-        </div>
-        <div class="game__option  game__option--selected">
-          <img src=${level.images[1].url} alt="Option 2" width="304" height="455">
-        </div>
-        <div class="game__option">
-          <img src=${level.images[2].url} alt="Option 3" width="304" height="455">
-        </div>
-      </form>`;
-    default:
-      return ``;
+  const levelType = level.type;
+  const frameSize = levelTypes[levelType].imageSize;
+  let contentClass = `game__content`;
+  contentClass += (levelType === `images-1`) ? ` game__content--wide` : ``;
+  contentClass += (levelType === `images-3`) ? ` game__content--triple` : ``;
+
+  return `<p class="game__task">${level.text}</p>
+  <form class="${contentClass}">
+  ${level.images.map((image, index) => {
+    const size = resize(frameSize, image.size);
+    const imageElement = `<img src=${image.url} alt="Option ${index + 1}" width="${size.width}" height="${size.height}">`;
+
+    if (levelType === `images-3`) {
+      return `<div class="game__option">
+        ${imageElement}
+      </div>`;
+    }
+
+    return `<div class="game__option">
+      ${imageElement}
+      <label class="game__answer game__answer--photo">
+        <input class="visually-hidden" name="question${index + 1}" type="radio" value="photo">
+        <span>Фото</span>
+      </label>
+      <label class="game__answer game__answer--paint">
+        <input class="visually-hidden" name="question${index + 1}" type="radio" value="paint">
+        <span>Рисунок</span>
+      </label>
+    </div>`;
   }
+  ).join(``)}
+  </form>`;
 };
 
 export default renderGame;
